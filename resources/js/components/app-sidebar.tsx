db4f44dce11242a466a8f4cce8a1b3a9b7a2ac6f
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-09-03 19:45:06 +0800

    add category feature

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Database, KeySquare, LayoutGrid, Users } from 'lucide-react';
+import { BookOpen, Database, Grid2X2, KeySquare, LayoutGrid, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -17,6 +17,11 @@ const mainNavItems: NavItem[] = [
     title: 'Documentation',
     href: route('documentation'),
     icon: BookOpen,
+  },
+  {
+    title: 'Categories',
+    href: route('category.index'),
+    icon: Grid2X2,
   },
 ];
 
